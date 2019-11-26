07b3a8862035699c8745aa6f3cccd878ef05de56
Vincent Andrieu <vincent.andrieu@epietch.eu>
2019-11-26 18:41:52 +0100

    - Add my_get_pixel_color in the graph lib

--- include/graph.h
+++ include/graph.h
@@ -30,13 +30,15 @@ framebuffer_t *framebuffer_create(unsigned int width, unsigned int height);
 void framebuffer_destroy(framebuffer_t *framebuffer);
 void my_put_pixel(framebuffer_t *framebuffer, unsigned int x, unsigned int y,
                     sfColor color);
-void my_draw_rect(framebuffer_t *buffer, sfVector2f coords,
-                    sfVector2f size, sfColor color);
+void my_draw_rect(framebuffer_t *buffer, sfVector2f coords, sfVector2f size,
+                    sfColor color);
 void my_draw_line(framebuffer_t *framebuffer, sfVector2f point_a,
                     sfVector2f point_b, sfColor color);
-void my_draw_rhombus(framebuffer_t *framebuffer, sfVector2f coord,
-                    int radius, sfColor color);
-void my_draw_circle(framebuffer_t *framebuffer, sfVector2f coord,
-                    int radius, sfColor color);
+void my_draw_rhombus(framebuffer_t *framebuffer, sfVector2f coord, int radius,
+                    sfColor color);
+void my_draw_circle(framebuffer_t *framebuffer, sfVector2f coord, int radius,
+                    sfColor color);
+sfColor my_get_pixel_color(framebuffer_t *framebuffer, unsigned int x,
+                    unsigned int y);
 
 #endif
