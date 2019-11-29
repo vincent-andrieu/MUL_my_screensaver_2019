25d982244643959fbb524f22b35f4d397f900b2d
Vincent Andrieu <vincent.andrieu@epietch.eu>
2019-11-29 19:59:01 +0100

    - fix style in my_draw_circle lib function

--- lib/graph/src/my_draw_circle.c
+++ lib/graph/src/my_draw_circle.c
@@ -18,9 +18,13 @@ void my_draw_circle(framebuffer_t *framebuffer, sfVector2f coord,
     for (float angle = 0; angle < 90; angle += 0.1) {
         x = radius * cos(angle * M_PI / 180);
         y = radius * sin(angle * M_PI / 180);
-      my_draw_line(framebuffer, (sfVector2f) {coord.x + x, coord.y + y}, (sfVector2f) {coord.x + x, coord.y}, color);
-      my_draw_line(framebuffer, (sfVector2f) {coord.x - x, coord.y - y}, (sfVector2f) {coord.x - x, coord.y}, color);
-      my_draw_line(framebuffer, (sfVector2f) {coord.x - x, coord.y + y}, (sfVector2f) {coord.x - x, coord.y}, color);
-      my_draw_line(framebuffer, (sfVector2f) {coord.x + x, coord.y - y}, (sfVector2f) {coord.x + x, coord.y}, color);
+        my_draw_line(framebuffer, (sfVector2f) {coord.x + x, coord.y + y},
+                    (sfVector2f) {coord.x + x, coord.y}, color);
+        my_draw_line(framebuffer, (sfVector2f) {coord.x - x, coord.y - y},
+                    (sfVector2f) {coord.x - x, coord.y}, color);
+        my_draw_line(framebuffer, (sfVector2f) {coord.x - x, coord.y + y},
+                    (sfVector2f) {coord.x - x, coord.y}, color);
+        my_draw_line(framebuffer, (sfVector2f) {coord.x + x, coord.y - y},
+                    (sfVector2f) {coord.x + x, coord.y}, color);
     }
 }
